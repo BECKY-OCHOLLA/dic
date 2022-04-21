80ed24a16eba440e893f56ba02c448cede4838d0
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-21 13:00:13 +0300

    created a variable for email input

--- loise.py
+++ loise.py
@@ -1,1 +1,13 @@
-home = { 'table'}
+# thisdis = {
+#     "jina" : "Loise",
+#     "age" : 23,
+#     "favourite-movie" : "finding Nemo"
+# }
+# print(thisdis)
+
+us = input(f'hi, what is your email address...... ?')
+
+print(us)
+
+
+
