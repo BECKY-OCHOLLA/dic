9913465f07c21a1e86913dfe12f88c8c394f57a6
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-21 15:18:45 +0300

    please be final touches

--- kelly.py
+++ kelly.py
@@ -18,13 +18,21 @@ age = input(f"Thanks for trusting me with that {name}... and how old are you?\n\
 person = {
     'jina':'',
     'age': '',
-    'favorite_movie':'spiderman going home'
+    'favorite_movie':''
 }
 
 person['jina'] = {name}
 person['age'] = {age}
 
+
+favorite_movie = input("What movie would you re-watch a million times?\n\n")
+
+print('Thats not a bad pick btw...\n\n')
+
 email_address_que = input(f'Mind giving us your email address?\n\n')
+
+
+person['favorite_movie'] = {favorite_movie}
 
 if email_address_que == 'no' or email_address_que == 'No' or email_address_que == 'NO' :
     email_address = input(f"\n\nNice...What is it?\n\n")
@@ -42,7 +50,7 @@ if email_address:
     if docstring == 'yes' or docstring == 'YES' or docstring == 'Yes' :
         print('You have good taste boy')
     else:
-        print('bruuuuuuuh!!!!??\n\n')
+        print('bruuuuuuuh!!!!?? \nWhy would you even lie to a robot?\n\n')
 else:
     print('get out!!\n\n')
     
